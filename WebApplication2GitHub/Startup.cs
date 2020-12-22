b48048caca595a6561478417445ccddfbc96517b
Saurabh Seth <saurabhseth654@gmail.com>
2020-12-22 21:15:32 +0530

    main commit

---
+++ WebApplication2GitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication2GitHub.Startup))]
+namespace WebApplication2GitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
